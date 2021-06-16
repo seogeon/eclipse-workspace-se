0a91a95bbe0f54f5f9e4fd473b55ef716d6b9223
WINDOWS10 20H1 X64 <WINDOWS10 20H1 X64@DESKTOP-M5VN126>
2021-06-16 18:13:59 +0900

    ..

---
+++ 10.객체간의관계[상속]-오버라이딩/src/AcademyStaff.java
@@ -0,0 +1,26 @@
+public class AcademyStaff extends AcademyMember {
+	public String depart;//부서
+	public AcademyStaff() {
+	}
+	//alt+shift+s --> o
+	public AcademyStaff(int no,String name,String depart) {
+		this.no=no;
+		this.name=name;
+		this.depart = depart;
+	}
+	
+	public void staffPrint() {
+		this.print();
+		System.out.println(depart);
+	}
+	
+	//alt+shift+s --> r
+	public String getDepart() {
+		return depart;
+	}
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	
+	
+}

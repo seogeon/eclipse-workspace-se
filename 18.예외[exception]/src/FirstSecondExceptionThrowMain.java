808dd58c6f3c8ff1d9272f116d60e6681c1293ae
STU <STU@DESKTOP-QOP6JBU>
2021-06-22 15:32:28 +0900

    ...

--- 18.예외[exception]/src/FirstSecondExceptionThrowMain.java
+++ 18.예외[exception]/src/FirstSecondExceptionThrowMain.java
@@ -1,13 +1,14 @@
+import java.io.IOException;
 
 public class FirstSecondExceptionThrowMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		System.out.println("1.main 흐름 start");
-		First first  = new First();
-        System.out.println("2.main 흐름 first.method1()호출");
-        first.method1();
-        System.out.println("3.main 흐름 first.method1()JVM반환");
-        return;
+		First first =new First();
+		System.out.println("2.main 흐름 first.method1() 호출전");
+		first.method1();
+		System.out.println("3.main 흐름 first.method1()JVM 반환");
+		return;
 	}
 
 }
